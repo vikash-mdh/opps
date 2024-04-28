1084d8232dd3cb7392e9ad7a76fbe8b323e170f9
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-28 23:17:34 +0530

    add ites

---
+++ src/main/java/org/OPPS/Main.java
@@ -0,0 +1,16 @@
+package org.OPPS;
+public class Main {
+    public static void main(String[] args) {
+
+       // Store 5 roll no
+        int [] num = new int[5];
+
+        // store 5 names
+        String []name = new String[6];
+
+        // data of five student :{roll no, name, marks};
+
+        int rno[] = new int[5];
+        String [] naam = new String[5];
+    }
+}
