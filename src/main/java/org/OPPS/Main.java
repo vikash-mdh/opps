331b3ad3559f009001907027ed1e60bafc127b40
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-29 08:41:44 +0530

    add some property

--- src/main/java/org/OPPS/Main.java
+++ src/main/java/org/OPPS/Main.java
@@ -1,16 +1,38 @@
 package org.OPPS;
+import java.util.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
        // Store 5 roll no
-        int [] num = new int[5];
+        //int [] num = new int[5];
 
         // store 5 names
-        String []name = new String[6];
+       // String []name = new String[6];
 
         // data of five student :{roll no, name, marks};
 
-        int rno[] = new int[5];
-        String [] naam = new String[5];
+//        int rno[] = new int[5];
+//        String [] naam = new String[5];
+//        float marks[] = new float[5];
+
+        Student[] student = new Student[5];
+
+        // just declearing value
+        Student vikash = new Student();
+        System.out.println(vikash.rno);
+        System.out.println(vikash.name);
+        System.out.println(vikash.marks);
+
     }
+}
+// create a class
+// for every student singlt student
+
+class Student{
+    int rno;
+    String name;
+    float marks;
 }
